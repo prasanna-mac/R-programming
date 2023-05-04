5cafdf074c5c7bf69183c6c3ecb032f41c85c2de
manchirevula <131148407+prasu-beshe@users.noreply.github.com>
2023-05-04 15:02:38 +0530

    Add files via upload

---
+++ task 3.0.R
@@ -0,0 +1,47 @@
+g Task's
+q1- create a 3°3 matrix
+ans:-
+mat<-matrix(c(1,2,3,4,5),nrow=4,ncol=3)
+print(mat) 
+
+
+42 create a array of 4°5°6
+ans:-
+kmics_array<-array(data=c(1,2,3,4,5,6,7),dim=c(4,5,6))
+print(kmics_array)
+
+
+
+
+gé-create vectors of char with your friend name
+ans:-
+names<-c("sanjana","prasanna","sharanya")
+print(names)
+c
+g5~- create a martix of range(1,2,3/4,5,6,7) with 4*5
+ans:-mat<-matrix(c(1,2,3,4,5,6,7),nrow=4,ncol=5)
+print(mat)
+
+
+J6- create a plot graph for range 1t0 20
++7 perform addition, substraction and division with with if,else,elself statement
+s
+
+
+q9:- perform arrays of 5 matrix with 4*3 formats
+sanjana_array<-array(data=c(1,2,3,4,5),dim=c(4,5))
+print(sanjana_array)
+
+
+
+
+
+
+
+
+
+
+
+q10:- perform arrays of 4 matrix with 5*6 with char of your friends names
+
++-
